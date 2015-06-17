f032c63b28650e21acf1aaeb036914874c2b2cc6
Eric Jonese <ericnatejones@gmail.com>
2015-06-17 11:58:47 -0600

    api

--- frontend/app/app.js
+++ frontend/app/app.js
@@ -13,6 +13,6 @@ angular.module('myApp', [
 
     config(['$routeProvider', 'RestangularProvider', function ($routeProvider, RestangularProvider) {
         $routeProvider.otherwise({redirectTo: '/flows'});
-        RestangularProvider.setBaseUrl('/localhost:8001')
+        RestangularProvider.setBaseUrl('http://localhost:8001')
     }]);
 
